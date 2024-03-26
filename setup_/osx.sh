650a7d20944d024337c1e23195b98fcecad29d85
Aaron Kobayashi <aaron.kobayashi@gmail.com>
2024-03-26 12:18:02 -0500

    Wipe out the old config.

--- setup_/osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f ~/Library/LaunchAgents/pbcopy.plist
-rm -f ~/Library/LaunchAgents/pbpaste.plist
-cp LaunchAgents_/pbcopy.plist ~/Library/LaunchAgents/
-cp LaunchAgents_/pbpaste.plist ~/Library/LaunchAgents/
-launchctl load ~/Library/LaunchAgents/pbcopy.plist
-launchctl load ~/Library/LaunchAgents/pbpaste.plist
