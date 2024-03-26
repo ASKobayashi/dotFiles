60a96787e89799491346e61c5b4bcfd0db649084
Aaron Kobayashi <aaron.kobayashi@gmail.com>
2024-03-26 11:19:07 -0500

    Removing no longer used stuff

--- bash/vim.sh
+++
@@ -1,7 +0,0 @@
-if command -v nvim >/dev/null 2>&1; then
-	export EDITOR="nvim"
-else
-	export EDITOR="vim"
-fi
-
-alias vi > /dev/null 2>&1 || alias vi="$EDITOR"
--- bash_logout
+++
@@ -1,5 +0,0 @@
-# With X forwarding, these channels might be open.  If they are they can hang
-# the logout
-killall -q xclip
-killall -q xsel
-
